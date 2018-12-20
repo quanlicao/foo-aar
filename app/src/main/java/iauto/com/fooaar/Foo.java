d74bf1860db998d49fb9551527bcb5e5730b8740
caoquanli <caoquanli@pset.suntec.net>
2018-12-20 15:11:26 +0800

    add Foo class

---
+++ app/src/main/java/iauto/com/fooaar/Foo.java
@@ -0,0 +1,10 @@
+package iauto.com.fooaar;
+
+import android.util.Log;
+
+public class Foo {
+    private final static String TAG = "foo";
+    public void dump() {
+        Log.d(TAG,"I am foo");
+    }
+}
