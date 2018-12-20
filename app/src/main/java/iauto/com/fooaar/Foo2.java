b04b8a4617555ee53729a6267bc7f638f3d13d4a
caoquanli <caoquanli@pset.suntec.net>
2018-12-20 16:54:44 +0800

    update foo2

--- app/src/main/java/iauto/com/fooaar/Foo2.java
+++ app/src/main/java/iauto/com/fooaar/Foo2.java
@@ -5,6 +5,6 @@ import android.util.Log;
 public class Foo2 {
     private final  static  String TAG = "foo2";
     public void dump() {
-        Log.d(TAG,"I am foo2,just for test");
+        Log.d(TAG,"I am foo2");
     }
 }
